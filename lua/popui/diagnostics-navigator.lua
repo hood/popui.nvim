628d598c656ac38e556ce47014e5f1f3e880d530
Andrea Cappuccio <hood@null.net>
2022-11-01 15:35:49 +0100

    yup, and dianostics-navigator is done, too!

--- lua/popui/diagnostics-navigator.lua
+++ lua/popui/diagnostics-navigator.lua
@@ -1,19 +1,5 @@
-local popfix = require("popfix")
 local borders = require("popui/borders")
-
-local popupReference = nil
-
-local calculatePopupWidth = function(entries)
-	local result = 0
-
-	for index, entry in pairs(entries) do
-		if #entry > result then
-			result = #entry
-		end
-	end
-
-	return result + 5
-end
+local core = require("popui/core")
 
 local formatEntries = function(entries)
 	local formatItem = function(entry)
@@ -37,70 +23,15 @@ end
 
 local diagnosticsNavigator = function()
 	local entries = vim.diagnostic.get(0)
+	local originalWindowId = vim.api.nvim_get_current_win()
 
 	if entries == nil or vim.tbl_isempty(entries) then
 		return
 	end
 
-	assert(popupReference == nil, "Busy in other LSP popup.")
-
-	local commitChoice = function(choiceIndex)
-		vim.api.nvim_win_set_cursor(0, { entries[choiceIndex].lnum + 1, entries[choiceIndex].col })
-	end
-
-	local formattedEntries = formatEntries(entries)
-
-	popupReference = popfix:new({
-		width = calculatePopupWidth(formattedEntries),
-		height = #formattedEntries,
-		close_on_bufleave = true,
-		keymaps = {
-			i = {
-				["<Cr>"] = function(popup)
-					popup:close(function(sel)
-						commitChoice(sel)
-					end)
-					popupReference = nil
-				end,
-			},
-			n = {
-				["<Cr>"] = function(popup)
-					popup:close(function(sel)
-						commitChoice(sel)
-					end)
-					popupReference = nil
-				end,
-				["<C-o>"] = function(popup)
-					popup:close()
-					popupReference = nil
-				end,
-				["<C-c>"] = function(popup)
-					popup:close()
-					popupReference = nil
-				end,
-				["<Esc>"] = function(popup)
-					popup:close()
-					popupReference = nil
-				end,
-			},
-		},
-		callbacks = {
-			close = function()
-				popupReference = nil
-			end,
-		},
-		mode = "cursor",
-		list = {
-			numbering = true,
-			border = true,
-			border_chars = borders[vim.g.popui_border_style or "rounded"],
-		},
-		data = formattedEntries,
-	})
-
-	if popupReference == false then
-		popupReference = nil
-	end
+	core:spawnListPopup(formatEntries(entries), function(lineNumber, lineContent)
+		vim.api.nvim_win_set_cursor(originalWindowId, { entries[lineNumber].lnum + 1, entries[lineNumber].col })
+	end)
 end
 
 return diagnosticsNavigator
